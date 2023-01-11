a2b083687e30c180d5672538e6f34588281b06fe
lcouedor <leo.couedor@gmail.com>
2023-01-11 12:03:43 +0100

    fusion

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-let selectedObject = 0;
+window.addEventListener("DOMContentLoaded", (event) => {
 
-let obj = [
-    {name: 'tableau', price : '500'},
-    {name: 'chaise', price : '100'},
-    {name: 'lit', price : '1000'},
-    {name: 'armoire', price : '2000'}];
+    let selectedObject = 0;
+
+    let obj = [
+        {name: 'tableau', price : '500'},
+        {name: 'chaise', price : '100'},
+        {name: 'lit', price : '1000'},
+        {name: 'armoire', price : '2000'}];
 
 
-let divProduit = document.getElementById("produit");
-let divResult = document.getElementById("result")
-let btnValide = document.getElementById("verif");
+    let divProduit = document.getElementById("produit");
+    let divResult = document.getElementById("result")
+    let btnValide = document.getElementById("verif");
 
-function HigherOrLower(price) {
-    if(obj[selectedObject].price > price) {
-        divResult.innerHTML="C'est moins" // true si supérieur
-    } else if(obj[selectedObject].price < price) {
-        divResult.innerHTML="C'est plus" // false si inférieur
-    } else if(obj[selectedObject].price == price) {
-        divResult.innerHTML="Oui oui oui, c'est gagné !" // egal si égal
-        btnValide.disabled = "true";
+    function HigherOrLower(price) {
+        // console.log("prix:", price)
+        console.log(obj[selectedObject].price > price)
+        if(obj[selectedObject].price > price) {
+            divResult.innerHTML="C'est plus" // true si supérieur
+        } else if(obj[selectedObject].price < price) {
+            divResult.innerHTML="C'est moins" // false si inférieur
+        } else if(obj[selectedObject].price == price) {
+            divResult.innerHTML="Oui oui oui, c'est gagné !" // egal si égal
+            btnValide.disabled = "true";
+        }
     }
-}
 
-function init(){
-    divProduit.innerHTML = obj[selectedObject].name
-}
+    function init(){
+        divProduit.innerHTML = obj[selectedObject].name
+    }
 
-function Guess(){
-    let price = document.getElementById('prix');
-    HigherOrLower(price);
-}
-init();
-btnValide.addEventListener("click",Guess);
+    function Guess(){
+
+        let price = document.getElementById('prix').value;
+        HigherOrLower(price);
+    }
+    init();
+    btnValide.addEventListener("click",Guess);
+
+});
